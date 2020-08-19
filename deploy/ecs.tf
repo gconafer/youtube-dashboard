16bc907262a0cb10c7615969033c1f3840e58326
문태준 <moon@muntaejun-ui-MacBookPro.local>
2020-08-19 18:55:17 +0900

    private key & id

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -43,6 +43,8 @@ data "template_file" "api_container_definitions" {
     app_image         = var.ecr_image_api
     proxy_image       = var.ecr_image_proxy
     django_secret_key = var.django_secret_key
+    private_key_id    = var.private_key_id
+    private_key       = var.private_key
     db_host           = aws_db_instance.main.address
     db_name           = aws_db_instance.main.name
     db_user           = aws_db_instance.main.username
